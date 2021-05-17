efa9d5d6f793f4bf3d07a4ca585fb5540c574ba9
MinseokCHAE <minseok6739@naver.com>
2021-05-17 17:03:17 +0900

    first commit

---
+++ encapsulation/grade.py
@@ -0,0 +1,23 @@
+class Grade() :
+    def setGrade(self, Kor, Eng, Math):
+        self.Kor = Kor
+        self.Eng = Eng
+        self.Math = Math
+
+    def sum(self):
+        return self.Kor + self.Eng + self.Math
+
+    def avr(self):
+        return  self.sum() / 3
+
+
+
+if __name__ == '__main__':
+    a = Grade()
+    a.setGrade(99, 98, 97)
+
+
+    print(a.sum())
+    print(a.avr())
+
+
