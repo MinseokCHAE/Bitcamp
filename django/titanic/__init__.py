1a7ae38b58d232df8969aa30d8b4343ed51bd1d1
MinseokCHAE <minseok6739@naver.com>
2021-05-31 17:56:16 +0900

    update_0531_17:56

--- django/titanic/__init__.py
+++ django/titanic/__init__.py
@@ -1,17 +1,36 @@
 from titanic.views.controller import Controller
+from titanic.templates.plot import Plot
 
 
 if __name__ == '__main__':
-    c = Controller()
 
     while 1:
-        menu = int(input('0.exit 1.print'))
+        menu = int(input('0.exit\n'
+                         '1.data visualization\n'
+                         '2.modeling\n'
+                         '3.machine learning\n'
+                         '4.machine release'))
         if menu == 0:
             break
 
         elif menu == 1:
-            c.preprocessing('train.csv')
+            Plot('train.csv').draw_survived_dead()
+            Plot('train.csv').draw_sex()
+            Plot('train.csv').draw_pclass()
+            Plot('train.csv').draw_embarked()
+
+        elif menu == 2:
+            Controller().modeling('train.csv', 'test.csv')
+
+        elif menu == 3:
+            pass
+
+        elif menu == 4:
+            pass
 
         else:
             print('잘못된입력')
             continue
+
+
+
--- django/titanic/views/controller.py
+++ django/titanic/views/controller.py
@@ -7,14 +7,20 @@ class Controller(object):
     d = Dataset()
     s = Service()
 
-    def modeling(self, train) -> object:
+    def modeling(self, train, test) -> object:
         s = self.s
+        this = self.preprocessing(train, test)
+        this.label = s.create_label(this)
+        this.train = s.create_train(this)
+        return this
 
-    def preprocessing(self, train) -> object:
+    def preprocessing(self, train, test) -> object:
         s = self.s
         this = self.d
         this.train = s.new_model(train)
+        this.test = s.new_model(test)
         print(f'train type : {type(this.train)}')
         print(f'train column : {this.train.columns}')
-        print(f'train 상위5개 data : {this.train.head()}')
-        print(f'train 하위5개 data : {this.train.tail()}')
+        print(f'test type : {type(this.test)}')
+        print(f'test column : {this.test.columns}')
+        return this
